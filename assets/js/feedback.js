c00276e2366ac4bcde1577b2c2a14c9aaf6efe79
nuclearbanane <rodger.waldron@gmail.com>
2016-12-29 15:40:35 -0500

    Now supports multiple TAs and works with #25 Course Code and Section still need to be gotten dynamically

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,17 +1,20 @@
+var tas= [];
 $( ".course_select" ).change(function() {
     if($(".course_select").val() == "None") {
         $('.sections_list').html('');
         return;
     }
-    $.post("/getSections",{ coursecode : $(".course_select").val() },
+	var course_code = $(".course_select").val();
+    $.post("/getSections",{ coursecode : course_code },
         function(data, status){
             var sections_list = $(".sections_list");
             $('#section_code').val("");
             sections_list.html('');
+			alert(data);
             data = $.parseJSON(data);
-            for(let time of data.results){
-                sections_list.append(time);
-                sections_list.append($("<input class=\"section_select\"type=\"radio\" name=\"section\" value=\""+time+"\" />"));
+            for(let section of data.results){
+                sections_list.append(course_code+section[0]+": "+section[1]+" - "+section[2]);
+                sections_list.append($("<input class=\"section_select\"type=\"radio\" name=\"section\" value=\""+section[0]+"\" />"));
                 sections_list.append("<br>");
             }
             $('#course_code').val($(".course_select").val());
@@ -22,12 +25,85 @@ $(document.body).on('change','.section',function() {
     if ($("input[name='section']").is(':checked')) {
         $('.feedback').show();
         $('#section_code').val($("input[name='section']:checked").val());
+		//TODO: Generate input data from selection
+		input = { course:"CEG2136", section:"T1"};
+		$.post("/getSectionTAs",input,function(data,status){
+			alert(data);
+			data = $.parseJSON(data);
+			var fields = $(".fields");
+			fields.html('');
+			for(let ta of data.TAs){
+				alert(ta.name);
+				var name = ta.name.replace(/\s/g, '');
+
+				tas.push({name:ta.name,id:ta.taID});
+				fields.append("<h3>"+ta.name+"</h3>");
+				a = "<table>"+
+"					<tr>"+
+"						<th>How often was the TA prepared?</th><td>"+
+"						<select id='"+name+"range_1'name='range_1'>"+
+"							<option value=1> Never          </option>"+
+"							<option value=2> Sometimes      </option>"+
+"							<option value=3> Usually        </option>"+
+"							<option value=4> Almost Always  </option>"+
+"							<option value=5> Always         </option>"+
+"						</select></td>"+
+"					</tr>"+
+"					<tr>"+
+"						<th>How well does the TA know the subject material?</th><td>"+
+"						<select id='"+name+"range_2' name='range_2'>"+
+"							<option value=1>Not at all          </option>"+
+"							<option value=2>Slacking Knownledge </option>"+
+"							<option value=3>Knows enough        </option>"+
+"							<option value=4>Knows the material  </option>"+
+"							<option value=5>Expert              </option>"+
+"						</select></td>"+
+"					</tr>"+
+"					<tr>"+
+"						<th>How relevant was lab/DGD material to class?</th>"+
+"						<td><select id='"+name+"range_3' name='range_3'>"+
+"							<option value=1>Not at all</option>"+
+"							<option value=2>A little</option>"+
+"							<option value=3>Sees some content</option>"+
+"							<option value=4>Sees most content</option>"+
+"							<option value=5>Perfect for content</option>"+
+"						</select></td>"+
+"					</tr>"+
+"					<tr>"+
+"						<th>Feedback about the TA</th>"+
+"						<td><textarea rows=10 cols=30 id='"+name+"feed' name='taFeedback'></textarea></td>"+
+"					</tr>"+
+"				</table>";
+				fields.append(a);
+			}
+			fields.append("<input type='submit' value='Submit' />");
+		})
     }
 });
-
-/*$('.feedback_form').on('submit',function(event){
-        event.preventDefault() ;
-        alert("Form Submission stoped.");
-        //TODO: Make AJAX
-        //TODO: Handle success properly
-});*/
+$(function(){
+	$('.feedback_form').on('submit',function(event){
+        	event.preventDefault() ;
+        	alert("Form Submission stoped.");
+			f = []
+			for(let ta of tas){
+				var name = ta.name.replace(/\s/g, '');
+				var feedback = {
+					taID : ta.id,
+					q1 : $(document.getElementById(name+"range_1")).val(),
+					q2 : $(document.getElementById(name+"range_2")).val(),
+					q3 : $(document.getElementById(name+"range_3")).val(),
+					feedback : $(document.getElementById(name+"feed")).val()
+				}
+				f.push(feedback);
+			};
+			data = {
+				student : $("input[name=studnum]").val(),
+				course : $("input[name=course_code]").val(),
+				section : $("input[name=section_code]").val(),
+				feedback : f
+			};
+			$.post("/submitFeedBack",data,function(data,status){
+				alert("Success");
+			});
+	});
+});
