a91d91f94f34b0d27182582f6ce920ffe7471420
Jules <julesmazur@gmail.com>
2017-01-26 07:43:51 -0500

    Adds test cases for simultaneous DGD sections.
    As described in #34 and separately in #41, this adds edge cases to
    `db_test_data.sql` where DGDs occur simultaneously with distinct
    sections, TAs, and (implicitly) locations.
    
    Causes expected behaviour and no unexpected side-effects; tentatively
    closes #34 and #41 pending review.

--- db_test_data.sql
+++ db_test_data.sql
@@ -1,6 +1,6 @@
-use feedback;
+--use feedback;
 INSERT INTO course(code)
-VALUES ('ITI1120'),('ITI1121'),('CEG2136');
+VALUES ('ITI1120'), ('ITI1121'), ('CEG2136'), ('ITI1100');
 
 INSERT INTO ta(stnum, firstName, lastName, profilepic) VALUES
 ('1','Ashwin','Pancakes','AuntJemima.logo'),
@@ -9,11 +9,13 @@ INSERT INTO ta(stnum, firstName, lastName, profilepic) VALUES
 ('4','Remi','Gel','Rems.png');
 
 INSERT INTO section(course, sectionID,currYear,semester, weekday, startTime, endTime) VALUES
-('ITI1120', 'T1',2016,1, '1', '11:30', '14:30'),
-('CEG2136', 'T1',2016,1, '1', '11:30', '14:30'),
-('ITI1120', 'T2',2016,1, '2', '14:30', '17:30'),
-('ITI1121', 'T1',2017,2, '1', '11:30', '14:30'),
-('ITI1121', 'T2',2017,2, '2', '14:30', '17:30');
+('ITI1120', 'T1', 2016, 1, '1', '11:30', '14:30'),
+('CEG2136', 'T1', 2016, 1, '1', '11:30', '14:30'),
+('ITI1120', 'T2', 2016, 1, '2', '14:30', '17:30'),
+('ITI1121', 'T1', 2017, 2, '1', '11:30', '14:30'),
+('ITI1121', 'T2', 2017, 2, '2', '14:30', '17:30'),
+('ITI1100', 'T1', 2017, 2, '3', '8:30', '10:00'),
+('ITI1100', 'T2', 2017, 2, '3', '8:30', '10:00');
 
 
 INSERT INTO teaches(taID, course, section, currYear, semester) VALUES
@@ -22,4 +24,6 @@ INSERT INTO teaches(taID, course, section, currYear, semester) VALUES
 ('4', 'CEG2136', 'T1', '2016', '1'),
 ('2', 'ITI1120', 'T2', '2016', '1'),
 ('1', 'ITI1121', 'T2', '2017', '2'),
-('3', 'ITI1121', 'T1', '2017', '2');
+('3', 'ITI1121', 'T1', '2017', '2'),
+('3', 'ITI1100', 'T1', '2017', '2'),
+('4', 'ITI1100', 'T2', '2017', '2');
